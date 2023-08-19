e0b70c25f629be243453a22002e8371dd43a1c59
KetchuppOfficial <dolgov04@list.ru>
2023-08-19 16:25:06 +0300

    End-to-end testing has been simplified

--- test/end_to_end/src/driver.cpp
+++ test/end_to_end/src/driver.cpp
@@ -3,14 +3,27 @@
 #include <fstream>
 #include <chrono>
 
+#ifdef STD_SET
+#include <set>
+#else
 #include "arb_tree.hpp"
+#endif
+
 #include "common.hpp"
 
 int main ()
 {
+    #ifdef STD_SET
+    std::set<int> tree;
+    #else
     yLab::ARB_Tree<int> tree;
+    #endif
 
+    #ifdef STD_SET
+    std::ofstream file{"ans.info"};
+    #else
     std::ofstream file{"driver.info"};
+    #endif
     auto start = std::chrono::high_resolution_clock::now();
 
     while (!std::cin.eof())
@@ -30,11 +43,23 @@ int main ()
                 break;
 
             case end_to_end::Queries::kth_smallest:
+            #ifdef STD_SET
+            {
+                auto it = tree.begin();
+                std::advance (it, key_ - 1);
+                std::cout << *it << " ";
+            }
+            #else
                 std::cout << *tree[key_] << " ";
+            #endif
                 break;
 
             case end_to_end::Queries::n_less_than_given:
+            #ifdef STD_SET
+                std::cout << std::distance (tree.begin(), tree.lower_bound (key_)) << " ";
+            #else
                 std::cout << tree.n_less_than (key_) << " ";
+            #endif
                 break;
 
             default:
